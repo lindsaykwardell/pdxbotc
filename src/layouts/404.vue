864302bf6af65cee5003a06fad947d0f07595283
Lindasy Wardell <three060@gmail.com>
2021-09-29 08:00:38 -0700

    More cleanup

--- src/layouts/404.vue
+++ src/layouts/404.vue
@@ -1,23 +1,68 @@
-<script setup lang="ts">
-const router = useRouter()
-const { t } = useI18n()
-</script>
-
 <template>
-  <main class="px-4 py-10 text-center text-teal-700 dark:text-gray-200">
-    <div>
-      <p class="text-4xl">
-        <carbon-warning class="inline-block" />
-      </p>
+  <div>
+    <div class="bg-gray-900 p-2 flex">
+      <div class="flex flex-col items-center justify-center w-64">
+        <img
+          class="h-20"
+          src="https://bloodontheclocktower.com/img/website/logo.png?rel=1591572107883"
+          alt="Blood on the Clocktower"
+        />
+        <span
+          class="text-xl font-piratesbay text-gray-300"
+        >UNOFFICIAL Portland, OR</span>
+      </div>
+      <nav
+        class="pt-2 flex-grow flex flex-col md:flex-row justify-center md:justify-end items-end md:items-center h-24 text-2xl"
+      >
+        <a
+          href="https://twitter.com/pdxbotc"
+          target="_blank"
+          class="py-1 px-4 text-white hover:text-red-600 transition duration-50 font-piratesbay"
+        >
+          <font-awesome-icon :icon="['fab', 'twitter']" />
+          <span class="hidden sm:inline pl-2 hover:underline">Twitter</span>
+        </a>
+        <a
+          href="https://www.facebook.com/groups/BOTCPDX"
+          target="_blank"
+          class="py-1 px-4 text-white hover:text-red-600 transition duration-50 font-piratesbay"
+        >
+          <font-awesome-icon :icon="['fab', 'facebook']" />
+          <span class="hidden sm:inline pl-2 hover:underline">Facebook</span>
+        </a>
+        <a
+          href="https://discord.gg/HMVv8rgFTY"
+          target="_blank"
+          class="py-1 px-4 text-white hover:text-red-600 transition duration-50 font-piratesbay"
+        >
+          <font-awesome-icon :icon="['fab', 'discord']" />
+          <span class="hidden sm:inline pl-2 hover:underline">Discord</span>
+        </a>
+        <a
+          href="mailto:pdxbotc@gmail.com"
+          target="_blank"
+          class="hidden sm:inline py-1 px-4 text-white hover:text-red-600 transition duration-50 font-piratesbay"
+        >
+          <font-awesome-icon :icon="['fa', 'envelope']" />
+          <span class="hidden sm:inline pl-2 hover:underline">Email</span>
+        </a>
+      </nav>
     </div>
     <router-view />
-    <div>
-      <button
-        class="btn m-3 text-sm mt-8"
-        @click="router.back()"
-      >
-        {{ t('button.back') }}
-      </button>
-    </div>
-  </main>
+  </div>
 </template>
+
+<style lang="postcss">
+@font-face {
+  font-family: PiratesBay;
+  src: url(/fonts/piratesbay.ttf);
+}
+
+body {
+  @apply bg-gray-700;
+}
+
+.prose * {
+  color: white !important;
+}
+</style>

50ace3d2e049e1448104480703276d940474d73e
Lindsay Wardell <three060@gmail.com>
2022-06-30 14:34:04 +0000

    add vite PWA back

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,28 @@
 import { defineConfig } from 'astro/config';
+import { VitePWA } from 'vite-plugin-pwa'
 
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  integrations: [vue(), tailwind()],
+  vite: {
+    plugins: [VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['/img/demon-head.png'],
+      manifest: {
+        name: 'Blood on the Clocktower | Unofficial Portland Group',
+        short_name: 'PDXBOTC',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/img/demon-head.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),]
+  }
 });

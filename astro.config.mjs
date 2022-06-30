5c6b624ae20f7d863f443b73f91b5fbcbde32c32
Lindsay Wardell <three060@gmail.com>
2022-06-30 14:44:34 +0000

    remove it (didn't work)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,9 @@
 import { defineConfig } from 'astro/config';
-import { VitePWA } from 'vite-plugin-pwa'
 
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()],
-  vite: {
-    plugins: [VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['/img/demon-head.png'],
-      manifest: {
-        name: 'Blood on the Clocktower | Unofficial Portland Group',
-        short_name: 'PDXBOTC',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/img/demon-head.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),]
-  }
+  integrations: [vue(), tailwind()]
 });

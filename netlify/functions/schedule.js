d98fa001cfda2d4e54c8ecb0c701fd9d4f545168
Lindsay Wardell <three060@gmail.com>
2023-01-27 15:25:04 -0800

    Update scheduler to generate monday and wednesday games

--- netlify/functions/schedule.js
+++ netlify/functions/schedule.js
@@ -10,31 +10,68 @@ dayjs.extend(utc);
 dayjs.extend(timezone);
 
 const myHandler = async (event, context) => {
-  const startDate = dayjs()
-    .add(1, "week")
-    .startOf("week")
-    .add(3, "day")
+  const startOfWeek = dayjs()
     .tz("America/Los_Angeles")
-    .add(18, "hour");
-  const endDate = startDate.add(4, "hour");
-  const newEvent = await axios
+    .startOf("day")
+    .add(1, "week")
+    .startOf("week");
+
+  const monEventStart = startOfWeek.add(1, "day").add(18, "hour");
+  const monEventEnd = monEventStart.add(4, "hour");
+
+  const wedEventStart = startOfWeek.add(3, "day").add(18, "hour");
+  const wedEventEnd = wedEventStart.add(4, "hour");
+
+  const mondayEvent = await axios
     .post(
       `https://discord.com/api/v10/guilds/${process.env.PUBLIC_SERVER_ID}/scheduled-events`,
       {
-        name: "Clocktower Online",
+        name: "Base Three Clocktower Online",
         privacy_level: "2",
-        scheduled_start_time: startDate.toISOString(),
-        scheduled_end_time: endDate.toISOString(),
-        description: `Join us in the virtual town square where we will once again avenge the horrid murder of our dear storyteller. 
+        scheduled_start_time: monEventStart.toISOString(),
+        scheduled_end_time: monEventEnd.toISOString(),
+        description: `Monday night we welcome newer players and storytellers looking to practice their ST chops. 
 
 We will be playing with the official application with the following Session ID: pdxbotc 
-      
+
 If this is your first time playing with the official app please do the following:
-      
+
 1 - Visit http://online.bloodontheclocktower.com/login and signup for an account. 
-      
+
 2 - Once you are logged in click “Setup Mic/Cam” to give permissions to the application to use those devices. 
-      
+
+3 - Press the letter “j” and join our Session ID: pdxbotc`,
+        entity_type: "3",
+        entity_metadata: {
+          location: "https://online.bloodontheclocktower.com/#pdxbotc",
+        },
+      },
+      {
+        headers: {
+          Authorization: `Bot ${process.env.SECRET_BOT_TOKEN}`,
+        },
+      }
+    )
+    .catch((err) => console.log(JSON.stringify(err.response.data, null, 2)));
+
+  const wednesdayEvent = await axios
+    .post(
+      `https://discord.com/api/v10/guilds/${process.env.PUBLIC_SERVER_ID}/scheduled-events`,
+      {
+        name: "Advanced Clocktower Online",
+        privacy_level: "2",
+        scheduled_start_time: wedEventStart.toISOString(),
+        scheduled_end_time: wedEventEnd.toISOString(),
+        description: `Wednesday night we challenge experienced players with funky custom scripts and experimental roles. 
+
+We will be playing with the official application with the following Session ID: pdxbotc 
+
+If this is your first time playing with the official app please do the following:
+
+1 - Visit http://online.bloodontheclocktower.com/login and signup for an account. 
+
+2 - Once you are logged in click “Setup Mic/Cam” to give permissions to the application to use those devices. 
+
 3 - Press the letter “j” and join our Session ID: pdxbotc`,
         entity_type: "3",
         entity_metadata: {
@@ -50,8 +87,11 @@ If this is your first time playing with the official app please do the following
     .catch((err) => console.log(JSON.stringify(err.response.data, null, 2)));
   return {
     statusCode: 200,
-    body: JSON.stringify(newEvent.data),
+    body: [
+      JSON.stringify(mondayEvent.data),
+      JSON.stringify(wednesdayEvent.data),
+    ],
   };
 };
 
-exports.handler = schedule("0 0 * * 4", myHandler);
+exports.handler = schedule("@weekly", myHandler);

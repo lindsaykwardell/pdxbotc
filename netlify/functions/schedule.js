29e2283d12d7f0442c4ae1680b6ed507e129c043
Lindsay Wardell <three060@gmail.com>
2023-02-04 21:41:57 -0800

    change time

--- netlify/functions/schedule.js
+++ netlify/functions/schedule.js
@@ -94,4 +94,4 @@ If this is your first time playing with the official app please do the following
   };
 };
 
-exports.handler = schedule("0 0 * * 1", myHandler);
+exports.handler = schedule("0 8 * * 4", myHandler);

a237a8b3407cd02c542ef7ca00135075e65947f5
Lindsay Wardell <three060@gmail.com>
2023-03-10 15:50:40 +0000

    Add redirect link

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
+  output: 'server',
   integrations: [vue(), tailwind()],
 });
